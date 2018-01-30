7de266ee6f14f7f49fe4a5ae54110b8de9800558
TheBigA <AFactor@users.noreply.github.com>
2018-01-30 15:53:40 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+
+var 
+  util = require("util"),
+  fs = require('fs'),
+	clc = require('cli-color'),
+	cp = require('glob-copy'),
+    yaml = require('yamljs');
+	glob = require("glob");
+	
+
+
+	
+
+ 
+
+	var info = function(data){
+		console.log(clc.green(data));
+	}
+
+	var warn = function(data){
+		console.log(clc.yellow(data));
+	}
+
+	var error = function(data){
+		console.log(clc.red(data));
+	}
+
+	var debug = function(data){
+		console.log(clc.red.bgWhite.underline(data));
+	}
+
+  info('data');
+  
+
